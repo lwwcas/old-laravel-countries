635013b766b73766d917fe6d3394d22fc06f9945
Lucas Duarte <lwwcas@gmail.com>
2019-08-24 22:16:59 +0100

    feat: create EuropeSeeder file

---
+++ src/Database/Seeders/Lang/En/EuropeSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Lwwcas\LaravelCountries\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class EuropeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
